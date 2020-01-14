ac265bb3aa9c8ef05ee241cfc0e838c96fdf9fdb
aershov <codumentary.com@gmail.com>
2020-01-15 00:57:10 +0300

    add blend

--- src/main.kt
+++ src/main.kt
@@ -1,23 +1,35 @@
 import java.io.File
 
+fun color(ray: Ray): Vec3 {
+    val unitDirection = ray.direction.makeUnitVector()
+    val t = 0.5 * (unitDirection.y + 1)
+    return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+}
+
 fun main() {
     File("scene.ppm").printWriter().use { out ->
         val nx = 200
         val ny = 100
         out.print("P3\n$nx $ny\n255\n")
+        val lowerLeft = Vec3(-2.0, -1.0, -1.0)
+        val horizontal = Vec3(4.0, 0.0, 0.0)
+        val vertical = Vec3(0.0, 2.0, 0.0)
+        val origin = Vec3(0.0, 0.0, 0.0)
         for (j in ny - 1 downTo 0) {
             for (i in 0 until nx) {
-                val col = Vec3((i.toDouble() / (nx - 1)), (j.toDouble() / (ny - 1)), 0.2)
-                val color = col * 255.0
-                val r = color.r().toInt()
-                val g = color.g().toInt()
-                val b = color.b().toInt()
+                val u = i.toDouble() / nx
+                val v = j.toDouble() / ny
+                val ray = Ray(origin, lowerLeft + u * horizontal + v * vertical)
+                val col = color(ray)
+                val r = (col.r() * 255.99).toInt()
+                val g = (col.g() * 255.99).toInt()
+                val b = (col.b() * 255.99).toInt()
                 out.print("$r $g $b\n")
             }
         }
     }
 }
 
-private operator fun Double.times(color: Vec3): Vec3 {
-    return color * this
+private operator fun Double.times(vec: Vec3): Vec3 {
+    return vec * this
 }
