32f228047b81f292494f15499fb99f823228788d
aershov <codumentary.com@gmail.com>
2020-01-18 19:28:20 +0300

    use HitableList

--- src/main.kt
+++ src/main.kt
@@ -2,10 +2,9 @@ import java.io.File
 import kotlin.math.sqrt
 
 
-fun color(ray: Ray): Vec3 {
+fun color(ray: Ray, world: Hitable): Vec3 {
     val unitDirection = ray.direction.makeUnitVector()
-    val sphere = Sphere(Vec3(0.0, 0.0, -1.0), 0.5)
-    val hit = sphere.hit(ray, 0.0, Double.MAX_VALUE)
+    val hit = world.hit(ray, 0.0, Double.MAX_VALUE)
     if (hit != null) {
         return (hit.normal + Vec3(1.0, 1.0, 1.0)) * 0.5
     }
@@ -22,12 +21,15 @@ fun main() {
         val horizontal = Vec3(4.0, 0.0, 0.0)
         val vertical = Vec3(0.0, 2.0, 0.0)
         val origin = Vec3(0.0, 0.0, 0.0)
+        val smallSphere = Sphere(Vec3(0.0, 0.0, -1.0), 0.5)
+        val largeSphere = Sphere(Vec3(0.0, -100.5, -1.0), 100.0)
+        val world = HitableList(listOf(smallSphere, largeSphere))
         for (j in ny - 1 downTo 0) {
             for (i in 0 until nx) {
                 val u = i.toDouble() / nx
                 val v = j.toDouble() / ny
                 val ray = Ray(origin, lowerLeft + u * horizontal + v * vertical)
-                val col = color(ray)
+                val col = color(ray, world)
                 val r = (col.r() * 255.99).toInt()
                 val g = (col.g() * 255.99).toInt()
                 val b = (col.b() * 255.99).toInt()
