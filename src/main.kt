1b1225f968f1cb38f98505c386df63e528f214d2
aershov <codumentary.com@gmail.com>
2020-01-25 20:26:00 +0300

    fixing dielectrics

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.random.Random.Default.nextDouble
 
@@ -35,26 +36,40 @@ fun refract(v: Vec3, n: Vec3, ni_over_nt: Double): Vec3? {
     return null
 }
 
+fun schlick(cosine: Double, refIdx: Double): Double {
+    val r0 = (1 - refIdx) / (1 + refIdx).pow(2)
+    return r0 + (1 - r0) * (1 - cosine).pow(5)
+
+}
+
 
 data class Dielectric(val ri: Double): Material {
     override fun scatter(incident: Ray, hit: Hit): Scatter? {
         val outward: Vec3
         val reflected = reflect(incident.direction, hit.normal)
         val niOverNt: Double
+        val cosine: Double
         val attenuation = Vec3(1.0, 1.0, 1.0)
         if (incident.direction.dot(hit.normal) > 0) {
             outward = -hit.normal
             niOverNt = ri
+            cosine = ri * incident.direction.dot(hit.normal) / incident.direction.length()
         } else {
             outward = hit.normal
             niOverNt = 1.0 / ri
+            cosine = -incident.direction.dot(hit.normal) / incident.direction.length()
         }
         val refraction = refract(incident.direction, outward, niOverNt)
         if (refraction != null) {
+            val reflectProb = schlick(cosine, ri)
+            if (nextDouble() < reflectProb) {
+                return Scatter(Ray(hit.point, reflected), attenuation)
+            }
             return Scatter(Ray(hit.point, refraction), attenuation)
+        } else {
+            return Scatter(Ray(hit.point, reflected), attenuation)
         }
-        return null
-}
+    }
 }
 
 fun color(ray: Ray, world: Hitable, depth: Int): Vec3 {
@@ -121,9 +136,10 @@ fun main() {
 private fun makeWorld(): Hitable {
     val smallSphere = Sphere(Vec3(0.0, 0.0, -1.0), 0.5, Lambertian(Vec3(0.1, 0.2, 0.5)))
     val largeSphere = Sphere(Vec3(0.0, -100.5, -1.0), 100.0, Lambertian(Vec3(0.8, 0.8, 0.0)))
-    val firstMetal = Sphere(Vec3(1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.6, 0.2), 0.0))
-    val secondMetal = Sphere(Vec3(-1.0, 0.0, -1.0), 0.5, Dielectric(1.5))
-    return HitableList(listOf(smallSphere, largeSphere, firstMetal, secondMetal))
+    val metal = Sphere(Vec3(1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.6, 0.2), 0.0))
+    val firstDielectric = Sphere(Vec3(-1.0, 0.0, -1.0), 0.5, Dielectric(1.5))
+    val secondDielectric = Sphere(Vec3(-1.0, 0.0, -1.0), -0.45, Dielectric(1.5))
+    return HitableList(listOf(smallSphere, largeSphere, metal, firstDielectric, secondDielectric))
 }
 
 private operator fun Double.times(vec: Vec3): Vec3 {
