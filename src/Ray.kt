f8d8d64b5bc01e0eaf08a88d857c42f2b5694925
aershov <codumentary.com@gmail.com>
2020-02-02 09:44:52 +0300

    add time to Ray

--- src/Ray.kt
+++ src/Ray.kt
@@ -1,4 +1,4 @@
-data class Ray(val origin: Vec3, val direction: Vec3) {
+data class Ray(val origin: Vec3, val direction: Vec3, val time: Double = 0.0) {
     fun pointAtParameter(t: Double): Vec3 {
         return origin + direction * t
     }
