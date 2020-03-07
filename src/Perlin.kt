b9edbfbe3d8a91494bbed8003313ef2953d29efa
aershov <codumentary.com@gmail.com>
2020-03-07 20:36:41 +0300

    perlin with dot product

--- src/Perlin.kt
+++ src/Perlin.kt
@@ -1,26 +1,25 @@
 import kotlin.math.floor
-import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class Perlin {
     private val permX = perlinGeneratePerm()
     private val permY = perlinGeneratePerm()
     private val permZ = perlinGeneratePerm()
-    private val ranDouble = perlinGenerate()
+    private val ranVector = perlinGenerate()
 
     fun noise(p: Vec3): Double {
         val i = floor(p.x).toInt()
         val j = floor(p.y).toInt()
         val k = floor(p.z).toInt()
-        val u = hermit(p.x - i)
-        val v = hermit(p.y - j)
-        val w = hermit(p.z - k)
-        val c = Array(2) { Array(2) { Array(2) { 0.0 } } }
+        val u = p.x - i
+        val v = p.y - j
+        val w = p.z - k
+        val c = Array(2) { Array(2) { Array(2) { Vec3(0.0, 0.0, 0.0) } } }
         for (di in 0..1) {
             for (dj in 0..1) {
                 for (dk in 0..1) {
                     val idx = (permX[(i + di) and 255]) xor (permY[(j + dj) and 255]) xor (permZ[(k + dk) and 255])
-                    c[di][dj][dk] = ranDouble[idx]
+                    c[di][dj][dk] = ranVector[idx]
                 }
             }
         }
@@ -40,19 +39,25 @@ class NoiseTexture : Texture {
     }
 }
 
-private fun trilinearInterp(c: Array<Array<Array<Double>>>, u: Double, v: Double, w: Double): Double {
+private fun trilinearInterp(c: Array<Array<Array<Vec3>>>, u: Double, v: Double, w: Double): Double {
+    val uu = hermit(u)
+    val vv = hermit(v)
+    val ww = hermit(w)
     var accum = 0.0
     for (i in 0..1) {
         for (j in 0..1) {
             for (k in 0..1) {
-                accum += (i * u + (1 - i) * (1 - u)) * (j * v + (1 - j) * (1 - v)) * (k * w + (1 - k) * (1 - w)) * c[i][j][k]
+                val weightV = Vec3(u - i, v - j, w - k)
+                accum += (i * uu + (1 - i) * (1 - uu)) * (j * vv + (1 - j) * (1 - vv)) * (k * ww + (1 - k) * (1 - ww)) * (c[i][j][k].dot(
+                    weightV
+                ))
             }
         }
     }
     return accum
 }
 
-fun permute(items: Array<Int>): Unit {
+fun permute(items: Array<Int>) {
     for (i in items.size - 1 downTo 1) {
         val j = Random.nextInt(0, i)
         val tmp = items[j]
@@ -61,8 +66,16 @@ fun permute(items: Array<Int>): Unit {
     }
 }
 
-fun perlinGenerate(): Array<Double> {
-    return Array(256) { Random.nextDouble() }
+fun perlinGenerate(): Array<Vec3> {
+    return Array(256) { randomVector() }
+}
+
+fun randomVector(): Vec3 {
+    return Vec3(
+        -1.0 + Random.nextDouble() * 2.0,
+        -1.0 + Random.nextDouble() * 2.0,
+        -1.0 + Random.nextDouble() * 2.0
+    ).makeUnitVector()
 }
 
 fun perlinGeneratePerm(): Array<Int> {
