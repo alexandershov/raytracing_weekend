6cff4335f24a966023b95b2baf25b14292849567
aershov <codumentary.com@gmail.com>
2020-03-14 19:33:39 +0300

    generalize Rect

--- src/Rect.kt
+++ src/Rect.kt
@@ -1,10 +1,13 @@
+import java.lang.RuntimeException
+
 // plane == 0|1|2 == x|y|z
 data class Rect(val material: Material, val min: Vec3, val max: Vec3, val plane: Int): Hitable {
     override fun hit(ray: Ray, min_t: Double, max_t: Double): Hit? {
         val t = min[plane] - ray.origin[plane] / ray.direction[plane]
         val p = ray.pointAtParameter(t)
-        if ((p.x in min.x..max.x) and (p.y in min.y..max.y) and (t in min_t..max_t)) {
-            return Hit(t, p, normal(), (p.x - min.x) / (max.x - min.x), (p.y - min.y) / (max.y - min.y), material)
+        val inside = listOf(0, 1, 2).filter { d -> d != plane}.all { d -> p[d] in min[d]..max[d] }
+        if (inside and (t in min_t..max_t)) {
+            return Hit(t, p, normal(), getU(p), getV(p), material)
         }
         return null
     }
@@ -20,5 +23,21 @@ data class Rect(val material: Material, val min: Vec3, val max: Vec3, val plane:
         val result = Vec3(0.0, 0.0, 0.0)
         result[plane] = 1.0
         return result
+    }
+
+    private fun getU(p: Vec3): Double {
+        if (plane == 2) {
+            return (p.x - min.x) / (max.x - min.x)
+        } else {
+            throw RuntimeException()
+        }
+    }
+
+    private fun getV(p: Vec3): Double {
+        if (plane == 2) {
+            return (p.y - min.y) / (max.y - min.y)
+        } else {
+            throw RuntimeException()
+        }
     }
 }
