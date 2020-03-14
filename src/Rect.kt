5ae1277c0eb72de407a8fc44ef403f620da7f467
aershov <codumentary.com@gmail.com>
2020-03-14 19:25:39 +0300

    extract to normal()

--- src/Rect.kt
+++ src/Rect.kt
@@ -1,10 +1,10 @@
 // plane == 0|1|2 == x|y|z
 data class Rect(val material: Material, val min: Vec3, val max: Vec3, val plane: Int): Hitable {
     override fun hit(ray: Ray, min_t: Double, max_t: Double): Hit? {
-        val t = min.z - ray.origin.z / ray.direction.z
+        val t = min[plane] - ray.origin[plane] / ray.direction[plane]
         val p = ray.pointAtParameter(t)
         if ((p.x in min.x..max.x) and (p.y in min.y..max.y) and (t in min_t..max_t)) {
-            return Hit(t, p, Vec3(0.0, 0.0, 1.0), (p.x - min.x) / (max.x - min.x), (p.y - min.y) / (max.y - min.y), material)
+            return Hit(t, p, normal(), (p.x - min.x) / (max.x - min.x), (p.y - min.y) / (max.y - min.y), material)
         }
         return null
     }
@@ -16,4 +16,9 @@ data class Rect(val material: Material, val min: Vec3, val max: Vec3, val plane:
         )
     }
 
+    private fun normal(): Vec3 {
+        val result = Vec3(0.0, 0.0, 0.0)
+        result[plane] = 1.0
+        return result
+    }
 }
